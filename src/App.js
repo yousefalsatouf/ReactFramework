f1ea5e394809a160022cfa61b1150c5c0950e408
Yousefsatouf <yousef.alsatouf94@gmail.com>
2019-05-23 00:03:52 +0200

    some changements

--- src/App.js
+++ src/App.js
@@ -5,9 +5,27 @@ class App extends Component
 {
     render()
     {
+        const characters  = [
+            {
+                name: 'Charlie',
+                job: 'Janitor',
+            },
+            {
+                name: 'Mac',
+                job: 'Bouncer',
+            },
+            {
+                name: 'Dee',
+                job: 'Aspring actress',
+            },
+            {
+                name: 'Dennis',
+                job: 'Bartender',
+            },
+        ];
         return(
             <div className="App">
-              <Table/>
+                <Table characterData={characters}/>
             </div>
         )
     }
--- src/Table.js
+++ src/Table.js
@@ -11,38 +11,29 @@ const TableHeader = () =>
         </thead>
     )
 };
-const TableBody = () =>
+const TableBody = props =>
 {
-    return (
-        <tbody>
-        <tr>
-            <td>Charlie</td>
-            <td>Janitor</td>
-        </tr>
-        <tr>
-            <td>Mac</td>
-            <td>Bouncer</td>
-        </tr>
-        <tr>
-            <td>Dee</td>
-            <td>Aspiring actress</td>
-        </tr>
-        <tr>
-            <td>Dennis</td>
-            <td>Bartender</td>
-        </tr>
-        </tbody>
-    )
+    const rows = props.characterData.map((row, index) =>
+    {
+        return (
+            <tr key={index}>
+                <td>{rows.name}</td>
+                <td>{rows.job}</td>
+            </tr>
+        )
+    });
+    return <tbody>{rows}</tbody>;
 };
 
 class Table extends Component
 {
     render()
     {
+        const { characterData } = this.props;
         return (
             <table>
                 <TableHeader/>
-                <TableBody/>
+                <TableBody characterData={characterData}/>
             </table>
         )
     }
